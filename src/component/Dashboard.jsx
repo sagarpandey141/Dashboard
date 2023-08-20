c65e97eb64a8c4f8645dac53c6ebdd781065f653
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2023-08-20 16:52:58 +0530

    first commit

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div  className=' text-2xl bg-slate-600 text-red-500'>Dashboard</div>
+    <div>
+        
+    </div>
   )
 }
 
